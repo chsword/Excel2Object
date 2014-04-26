263f77e09c9413c144a3c3778f2400b87cce6fa4
chsword <chsword@126.com>
2014-04-26 11:45:32 +0800

    project init

---
+++ Chsword.Excel2Object.Tests/ExcelTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Chsword.Excel2Object.Tests.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Chsword.Excel2Object.Tests
+{
+    [TestClass]
+    public class ExcelTest
+    {
+        List<ReportModel> GetModels()
+        {
+            return new List<ReportModel>
+            {
+                new ReportModel{Name="a",Title="b"},
+                new ReportModel{Name="c",Title="d"},
+                new ReportModel{Name="f",Title="e"}
+            };
+        }
+
+        string GetFilePath(string file)
+        {
+            return Path.Combine(Environment.CurrentDirectory, file);
+        }
+        [TestMethod]
+        public void Test()
+        {
+            
+            var models = GetModels();
+            var exporter = new ExcelExporter();
+            var bytes = exporter.ObjectToExcelBytes(models);
+            var path = GetFilePath("text.xls");
+            Console.WriteLine(path);
+            File.WriteAllBytes(path, bytes);
+            Assert.IsTrue(File.Exists(path));
+            var importer = new ExcelImporter();
+            var result = importer.ExcelToObject<ReportModel>(path);
+            Assert.AreEqual(models.Count, result.Count());
+
+        }
+    }
+}
---
+++ Chsword.Excel2Object.Tests/Models/Report.cs
@@ -0,0 +1,10 @@
+﻿namespace Chsword.Excel2Object.Tests.Models
+{
+    public class ReportModel
+    {
+        [Excel("标题")]
+        public string Title { get; set; }
+        [Excel("用户")]
+        public string Name { get; set; }
+    }
+}
